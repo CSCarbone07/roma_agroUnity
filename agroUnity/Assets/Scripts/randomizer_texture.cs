cbd1f3e47f29cbd1324cb1bdc494f4849b85eb5c
CSCarbone07 <carlos.carbone@mailfence.com>
2022-08-10 09:54:53 +0200

    adding random ground textures

--- agroUnity/Assets/Scripts/randomizer_texture.cs
+++ agroUnity/Assets/Scripts/randomizer_texture.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class randomizer_texture : MonoBehaviour
 {
 
-    public string pathToTextures_RGB = "qr/numbers/";
+    public Material baseMaterial;
+    public string pathToTextures_RGB = "grounds/wageningen/sugarBeets/";
     //public string pathToTextures_NIR = "bonirob/weeds/nir/";
-
+    public int minIndexOfTextures = 1;
+    public int maxIndexOfTextures = 100;
+    private int currentTextureID = 0;
+    
+    private Material RGB_Mat_internal;
 
     // Start is called before the first frame update
     void Start()
     {
-           
+      setupMaterials();
+      changeTexture();
     }
 
     // Update is called once per frame
@@ -21,12 +27,19 @@ public class randomizer_texture : MonoBehaviour
         
     }
 
-    void changeTexture()
+    void setupMaterials()
     {
-	/*
-        RGB_Mat_internal = new Material(RGB_Mat);
+	RGB_Mat_internal = new Material(baseMaterial);
         //NIR_Mat_internal = new Material(NIR_Mat);
         //TAG_Mat_internal = new Material(TAG_Mat);
+
+    }
+
+    void changeTexture()
+    {
+        int randomName = Random.Range(minIndexOfTextures, maxIndexOfTextures);
+        currentTextureID = randomName;
+        
 
         Texture2D myRGBTexture = Resources.Load<Texture2D>(pathToTextures_RGB + currentTextureID) as Texture2D;
         //Texture2D myNIRTexture = Resources.Load<Texture2D>(pathToTextures_NIR + currentTextureID) as Texture2D;
@@ -34,9 +47,8 @@ public class randomizer_texture : MonoBehaviour
         RGB_Mat_internal.mainTexture = myRGBTexture; //("_MainTex", myTexture);
         //NIR_Mat_internal.mainTexture = myNIRTexture; //("_MainTex", myTexture);
         //TAG_Mat_internal.mainTexture = myRGBTexture; //("_MainTex", myTexture);
-
-        GetComponent<Renderer>().material = RGB_Mat_internal;
-	*/
+
+        GetComponent<Renderer>().material = RGB_Mat_internal;
     }
 
 }
